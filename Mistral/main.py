b467cc3d220a1c8f35d714df47605a033fa0c24d
Jakub Tehlar <tehlarjakub@gmail.com>
2025-03-01 19:53:06 +0100

    add structure

--- Mistral/main.py
+++ Mistral/main.py
@@ -6,6 +6,7 @@ from utils import *
 import argparse
 
 API_KEY="U3g8x7dBaIlNdoLnfgI81jyvtHLYZZCW"
+DEFAULT_MODEL="pixtral-12b-2409"
 
 
 # Path to your image
@@ -54,11 +55,16 @@ task_prompt: str = "The image displays an intelligence test question featuring a
 
 
 if __name__ == "__main__":
+    ##############################################################################################################
+    # Parse the arguments
+    ##############################################################################################################
     parser = argparse.ArgumentParser(description="Arguments specficying the configuration, dataset type and output path.")
 
     parser.add_argument("--dataset_type", type=str, help="The type of dataset to be used. Example: --dataset_type 3_comp", required=True)
     parser.add_argument("--config", type=str, help="The configuration of the dataset to be used. Example: --config oa_os_oc", required=True)
     parser.add_argument("--output_path", type=str, help="The path to save the output data. Example: --output_path output/3_comp", required=True)
+    parser.add_argument("--api_key", type=str, help="The API key for the Mistral API.", required=False, default=API_KEY)
+    parser.add_argument("--model", type=str, help=f"The model to be used for the Mistral API. The default model is set to {DEFAULT_MODEL}", required=False, default=DEFAULT_MODEL)
 
     ARGS = parser.parse_args()
     CONFIG = ARGS.config
@@ -72,13 +78,26 @@ if __name__ == "__main__":
     assert DATASET_TYPE in dataset_types, f"Invalid dataset type: {DATASET_TYPE}."
     assert OUTPUT_PATH is not None, "Output path not specified."
 
+    ##############################################################################################################
     # Load the data sheet
     # data/DATASET_TYPE/CONFIG/CONFIG_answers.csv
+    ##############################################################################################################
+
     data_file_path = "../Datasets/answer_sheets/data/"
     base_img_path = "../Datasets/3_comp/output_data/"
 
     data = load_csv(data_file_path + f"{DATASET_TYPE}/{CONFIG}/{CONFIG}_answers.csv") 
     for item in data:
         print(item)
+    
+
+    ##############################################################################################################
+    # Mistral API
+    ##############################################################################################################
+
+
+    ##############################################################################################################
+    # Save the answers
+    ##############################################################################################################
 
     
