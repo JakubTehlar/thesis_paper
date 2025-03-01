dd1acc1197f8ff70b882f0a0afee17bb1117b5fd
Jakub Tehlar <tehlarjakub@gmail.com>
2025-03-01 19:41:05 +0100

    add mistral interface

---
+++ Mistral/main.py
@@ -0,0 +1,84 @@
+import base64
+import requests
+import os
+from mistralai import Mistral
+from utils import *
+import argparse
+
+API_KEY="U3g8x7dBaIlNdoLnfgI81jyvtHLYZZCW"
+
+
+# Path to your image
+img_name = "oa_os_oc_center_single_RAVEN_9918_test.png"
+image_path = "../Datasets/3_comp/output_data/" + img_name
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+# Retrieve the API key from environment variables
+api_key = os.environ["MISTRAL_API_KEY"] if "MISTRAL_API_KEY" in os.environ else API_KEY
+
+# Specify model
+model = "pixtral-12b-2409"
+
+# Initialize the Mistral client
+client = Mistral(api_key=api_key)
+
+task_prompt: str = "The image displays an intelligence test question featuring a 3x3 grid with nine boxes, one of which is empty and marked with a question mark (?). Your task is to select the correct shape from six options (labeled A to F) to fill the empty box, completing the pattern that links all the shapes together. You must first give your explanation and then provide your answer at the end of your response in the format: 'The correct answer is: _'."
+
+# Define the messages for the chat
+# messages = [
+#     {
+#         "role": "user",
+#         "content": [
+#             {
+#                 "type": "text",
+#                 "text": task_prompt
+#             },
+#             {
+#                 "type": "image_url",
+#                 "image_url": f"data:image/jpeg;base64,{base64_image}" 
+#             }
+#         ]
+#     }
+# ]
+
+# # Get the chat response
+# chat_response = client.chat.complete(
+#     model=model,
+#     messages=messages
+# )
+
+# # Print the content of the response
+# print(chat_response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Arguments specficying the configuration, dataset type and output path.")
+
+    parser.add_argument("--dataset_type", type=str, help="The type of dataset to be used. Example: --dataset_type 3_comp", required=True)
+    parser.add_argument("--config", type=str, help="The configuration of the dataset to be used. Example: --config oa_os_oc", required=True)
+    parser.add_argument("--output_path", type=str, help="The path to save the output data. Example: --output_path output/3_comp", required=True)
+
+    ARGS = parser.parse_args()
+    CONFIG = ARGS.config
+    DATASET_TYPE = ARGS.dataset_type
+    OUTPUT_PATH = ARGS.output_path
+
+    configs = ["na_ns_nc", "na_ns_oc", "na_os_nc", "na_os_oc", "oa_ns_nc", "oa_ns_oc", "oa_os_nc", "oa_os_oc"]
+    dataset_types = ["3_comp", "4_comp"]
+
+    assert CONFIG in configs, f"Invalid configuration: {CONFIG}."
+    assert DATASET_TYPE in dataset_types, f"Invalid dataset type: {DATASET_TYPE}."
+    assert OUTPUT_PATH is not None, "Output path not specified."
+
+    # Load the data sheet
+    # data/DATASET_TYPE/CONFIG/CONFIG_answers.csv
+    data_file_path = "../Datasets/answer_sheets/data/"
+    base_img_path = "../Datasets/3_comp/output_data/"
+
+    data = load_csv(data_file_path + f"{DATASET_TYPE}/{CONFIG}/{CONFIG}_answers.csv") 
+    for item in data:
+        print(item)
+
+    
---
+++ Mistral/utils.py
@@ -0,0 +1,38 @@
+import base64
+import csv
+
+def encode_image(image_path):
+    """Encode the image to base64."""
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {image_path} was not found.")
+        return None
+    except Exception as e:  # Added general exception handling
+        print(f"Error: {e}")
+        return None
+    
+def save_csv(data, file_path):
+    """Save the data to a CSV file."""
+    try:
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+        print(f"Data saved to {file_path}")
+    except Exception as e:  # Added general exception handling
+        print(f"Error: {e}")
+
+def load_csv(file_path):
+    """Load data from a CSV file."""
+    try:
+        with open(file_path, mode='r') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+        return None
+    except Exception as e:  # Added general exception handling
+        print(f"Error: {e}")
+        return None
