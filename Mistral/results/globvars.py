5e11c4bf62f8fa112f8cf80c015375ee336e2532
Jakub Tehlar <tehlarjakub@gmail.com>
2025-03-14 11:16:59 +0100

    update

--- Mistral/results/globvars.py
+++ Mistral/results/globvars.py
@@ -4,6 +4,6 @@ PROMPTS=["default_prompt",
          "ft_prompt",
                             ]
 DATASETS=["3_comp",
-         "4_cmp"
+         "4_comp"
          ]
 CONFIGS = ["na_ns_nc", "na_ns_oc", "na_os_nc", "na_os_oc", "oa_ns_nc", "oa_ns_oc", "oa_os_nc", "oa_os_oc"]
