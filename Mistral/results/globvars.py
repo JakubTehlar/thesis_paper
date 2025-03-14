15e80414ab4eae7e97c85af4afb668f246579070
Jakub Tehlar <tehlarjakub@gmail.com>
2025-03-14 10:52:42 +0100

    create files

---
+++ Mistral/results/globvars.py
@@ -0,0 +1,9 @@
+MODELS=["pixtral-12b-2409",
+                            ]
+PROMPTS=["default_prompt",
+         "ft_prompt",
+                            ]
+DATASETS=["3_comp",
+         "4_cmp"
+         ]
+CONFIGS = ["na_ns_nc", "na_ns_oc", "na_os_nc", "na_os_oc", "oa_ns_nc", "oa_ns_oc", "oa_os_nc", "oa_os_oc"]
